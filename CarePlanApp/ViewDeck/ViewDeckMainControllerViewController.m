7044142729a00b94f5e02abbf01f283b59cd661c
namelessnerd <karthik.gomadam@gmail.com>
2013-11-16 11:35:03 -0800

    version of app that works

---
+++ CarePlanApp/ViewDeck/ViewDeckMainControllerViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewDeckMainControllerViewController.m
+//  CarePlanApp
+//
+//  Created by Karthik Gomadam on 7/26/13.
+//  Copyright (c) 2013 Accenture. All rights reserved.
+//
+
+#import "ViewDeckMainControllerViewController.h"
+
+@interface ViewDeckMainControllerViewController ()
+
+@end
+
+@implementation ViewDeckMainControllerViewController
+- (id)initWithCoder:(NSCoder *)aDecoder
+{
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"MainStoryboard" bundle:nil];
+    self = [super initWithCenterViewController:[storyboard instantiateViewControllerWithIdentifier:@"UserLandingViewController"]
+                            leftViewController:[storyboard instantiateViewControllerWithIdentifier:@"UserSettingsViewController"]];
+    
+
+    if (self) {
+        self.viewDeckController.leftSize= 100.0;
+        // Add any extra init code here
+    }
+    return self;
+}
+@end
